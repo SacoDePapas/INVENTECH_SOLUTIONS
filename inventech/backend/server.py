98b1f8b2eb8ae5c6c50aedbe517b53bb60ca48f6
SacoDePapas <sgomez15@alumnos.uaq.mx>
2025-02-26 10:16:00 -0600

    Finalizacion del crud

--- inventech/backend/server.py
+++ inventech/backend/server.py
@@ -33,7 +33,9 @@ SELECT_AREAS_UNO=("""SELECT * FROM Areas WHERE id = (%s)""")
 SELECT_USUARIOS=("""SELECT * FROM Usuarios  """)
 SELECT_USUARIOS_UNO=("""SELECT * FROM Usuarios  WHERE Id = (%s)""")
 SELECT_OBJETOS=("""SELECT * FROM Objetos """)
+SELECT_OBJETOS_UNO=("""SELECT * FROM Objetos  WHERE nombre = (%s)""")
 SELECT_RENTAS=("""SELECT * FROM Rentas """)
+SELECT_RENTAS_UNO=("""SELECT * FROM Rentas  WHERE Id = (%s)""")
 
 #UPDATE
 
@@ -105,7 +107,8 @@ def search_org():
         except Exception as e:
             return jsonify({"error": str(e)})
 
-
+#CRUD FACULTAD/AREAS ------------------------
+#FACULTAD
 @app.route("/create_facultad",methods=["POST"])
 def create_facultad():
     data=request.get_json()
@@ -116,8 +119,10 @@ def create_facultad():
     with connection.cursor() as cursor:
         nombre_facultad = data.get('nombre')
         id_org = data.get('org-code')
+        id_txt = data.get('id')
+        id = int(id_txt)
         try:
-            cursor.execute(SELECT_FACULTAD_UNO,(id_org,))
+            cursor.execute(SELECT_FACULTAD_UNO,(id,))
             Exist=cursor.fetchone()
             if not Exist:
                 cursor.execute(INSERT_FACULTAD,(id_org,nombre_facultad))
@@ -166,10 +171,79 @@ def update_facultad():
 
 
 
+#Areas
+@app.route("/create_Area",methods=["POST"])
+
+def create_Area():
+    data=request.get_json()
+    if not data:
+        app.logger.info(f"No data")
+        return jsonify({"error": "No data provided"}), 400
+
+    with connection.cursor() as cursor:
+        nombre_area = data.get('nombre')
+        id_org = data.get('org-code')
+        id_txt = data.get('id')
+        id=int(id_txt)
+        try:
+            cursor.execute(SELECT_AREAS_UNO,(id_org,))
+            Exist=cursor.fetchone()
+            if not Exist:
+                cursor.execute(INSERT_AREAS,(nombre_area,id_org))
+                connection.commit()
+                return {"Success":"Se creo el Area"}     
+            return {"Error":"Existe el Area"}
+        except Exception as e :
+            return jsonify({"error": str(e)})
+    return {"Nose":"nose"}
+
+@app.route("/delete_area",methods=["POST"])
+def delete_area():
+    data=request.get_json()
+    if not data:
+        app.logger.info(f"No data")
+        return jsonify({"error": "No data provided"}), 400
+
+    with connection.cursor() as cursor:
+        id_txt = data.get('Id')
+        id = int(id_txt)
+        try:
+            cursor.execute(DELETE_AREAS,(id))
+            cursor.commit()
+        except Exception as e :
+            return jsonify({"error": str(e)})
+    return {"Nose":"nose"}
+
+
+@app.route("/update_area",methods=["POST"])
+def update_area():
+    data=request.get_json()
+    if not data:
+        app.logger.info(f"No data")
+        return jsonify({"error": "No data provided"}), 400
+
+    with connection.cursor() as cursor:
+        id_txt = data.get('Id')
+        id = int(id_txt)
+        nombre = data.get('name')
+        id_org = data.get('id-org')
+        try:
+            cursor.execute(UPDATE_AREAS,(nombre,id_org,id))
+            cursor.commit()
+        except Exception as e :
+            return jsonify({"error": str(e)})
+    return {"Nose":"nose"}
 
 
 
 
+
+
+
+
+
+#CRUD USUARIOS/OBJETOS/RENTAS---------------
+#USUARIOS
 @app.route("/create_user",methods=["POST"])
 def create_user():
     data=request.get_json()
@@ -237,6 +311,142 @@ def update_user():
         except Exception as e :
             return jsonify({"error": str(e)})
     return {"Nose":"nose"}
+
+
+
+#OBJETOS
+@app.route("/create_objeto",methods=["POST"])
+def create_objeto():
+    data=request.get_json()
+    if not data:
+        app.logger.info(f"No data")
+        return jsonify({"error": "No data provided"}), 400
+
+    with connection.cursor() as cursor:
+        nombre = data.get('name')
+        desc = data.get('desc')
+        cant = int(data.get('cant'))
+        cant_disp = int(data.get('cant_disp'))
+        id_Area = int(data.get('id-area'))
+        Status= "Activo"
+        try:
+            cursor.execute(SELECT_OBJETOS_UNO,(nombre,))
+            Exist=cursor.fetchone()
+            if not Exist:
+                cursor.execute(INSERT_OBJETO,(nombre,desc,cant,cant_disp,id_Area,Status))
+                connection.commit()
+                return {"Success":"Se creo el Objeto"}     
+            return {"Error":"Existe el objeto"}
+        except Exception as e :
+            return jsonify({"error": str(e)})
+    return {"Nose":"nose"}
+
+@app.route("/delete_objeto",methods=["POST"])
+def delete_objeto():
+    data=request.get_json()
+    if not data:
+        app.logger.info(f"No data")
+        return jsonify({"error": "No data provided"}), 400
+
+    with connection.cursor() as cursor:
+        id_txt = data.get('Id')
+        id = int(id_txt)
+        try:
+            cursor.execute(DELETE_OBJETO,(id))
+            cursor.commit()
+        except Exception as e :
+            return jsonify({"error": str(e)})
+    return {"Nose":"nose"}
+
+
+@app.route("/update_objeto",methods=["POST"])
+def update_objeto():
+    data=request.get_json()
+    if not data:
+        app.logger.info(f"No data")
+        return jsonify({"error": "No data provided"}), 400
+
+    with connection.cursor() as cursor:
+        id_txt = data.get('Id')
+        id = int(id_txt)
+        nombre = data.get('name')
+        desc = data.get('desc')
+        cant = int(data.get('cant'))
+        cant_disp = int(data.get('cant_disp'))
+        id_Area = int(data.get('id-area'))
+        Status= "Activo"
+
+        try:
+            cursor.execute(UPDATE_OBJETO,(nombre,desc,cant,cant_disp,id_Area,Status,id))
+            cursor.commit()
+        except Exception as e :
+            return jsonify({"error": str(e)})
+    return {"Nose":"nose"}
+
+INSERT_RENTAS =("INSERT INTO Rentas (Id_usuario,Id_encargado,Salon,Id_Area,Status) VALUES (%s,%s,%s,%s,%s);")
+#Rentas
+@app.route("/create_rentas",methods=["POST"])
+def create_rentas():
+    data=request.get_json()
+    if not data:
+        app.logger.info(f"No data")
+        return jsonify({"error": "No data provided"}), 400
+
+    with connection.cursor() as cursor:
+        nombre = data.get('name')
+        id_objeto = int(data.get('id-objeto'))
+        id_usuario = int(data.get('id-user'))
+        id_encargado = int(data.get('id-encargado'))
+        salon = data.get('salon')
+        id_Area = int(data.get('id-area'))
+        Status= "Activo"
+        try:
+            cursor.execute(INSERT_RENTAS,(id_usuario,id_encargado,salon,id_Area,Status))
+            connection.commit()
+            return {"Success":"Se creo el Objeto"}     
+
+        except Exception as e :
+            return jsonify({"error": str(e)})
+    return {"Nose":"nose"}
+
+@app.route("/delete_objeto",methods=["POST"])
+def delete_objeto():
+    data=request.get_json()
+    if not data:
+        app.logger.info(f"No data")
+        return jsonify({"error": "No data provided"}), 400
+
+    with connection.cursor() as cursor:
+        id_txt = data.get('Id')
+        id = int(id_txt)
+        try:
+            cursor.execute(DELETE_RENTAS,(id))
+            cursor.commit()
+        except Exception as e :
+            return jsonify({"error": str(e)})
+    return {"Nose":"nose"}
+
+
+@app.route("/update_objeto",methods=["POST"])
+def update_objeto():
+    data=request.get_json()
+    if not data:
+        app.logger.info(f"No data")
+        return jsonify({"error": "No data provided"}), 400
+
+    with connection.cursor() as cursor:
+        id = int(data.get('id'))
+
+        Status= data.get('status')
+
+        try:
+            cursor.execute(UPDATE_RENTAS,(Status,id))
+            cursor.commit()
+        except Exception as e :
+            return jsonify({"error": str(e)})
+    return {"Nose":"nose"}
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
